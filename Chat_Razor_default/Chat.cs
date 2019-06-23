926d8b5c4edce46b811abb139cc2f3903d6b0c5f
Mani Shankar <mani.shankar@ericsson.com>
2019-06-23 20:15:59 +0530

    Razore first chat App

---
+++ Chat_Razor_default/Chat.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chat_Razor1
+{
+    public class Chat: Hub
+    {
+        public async Task Send(string message)
+        {
+            await Clients.All.SendAsync("Send", message);
+        }
+    }
+}
+
